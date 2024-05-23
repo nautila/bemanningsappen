148071aa8db819d967ded2a6d3332b3acee7125f
Bjørn Christian Moland <christianmoland@gmail.com>
2024-05-24 00:07:14 +0200

    feat(pages): adds dev page

---
+++ pages/dev.vue
@@ -0,0 +1,14 @@
+<template>
+	<EdgeDbAuthEmailSignup v-slot="{ email, updateEmail, password, updatePassword, submit, loading }" redirect-to="/">
+		<div>
+			<input type="email" :value="email" placeholder="Email" @change="(e: any) => updateEmail(e.target.value)" />
+			<input
+				type="password"
+				:value="password"
+				placeholder="Password"
+				@change="(e: any) => updatePassword(e.target.value)" />
+
+			<button type="button" @click.prevent="(e) => !loading && submit()">{{ loading ? "Laster" : "Registrer" }}</button>
+		</div>
+	</EdgeDbAuthEmailSignup>
+</template>
