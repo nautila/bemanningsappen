c5d26606e1859719fcb6c95ee0ca96f11778ec95
Bjørn Christian Moland <christianmoland@gmail.com>
2024-05-26 04:41:52 +0200

    feat(pages): adds body for employer page

--- pages/employer/[employerId]/index.vue
+++ pages/employer/[employerId]/index.vue
@@ -1,1 +1,21 @@
-<template></template>
+<script setup lang="ts">
+import type { ProfileInterface } from "~/types/employer/profile";
+
+const { data: profile, refresh } = await useFetch(`/api/employer/profile/profile`);
+
+const handleProfileUpdate = async (event: ProfileInterface) => {
+	const updated = await useFetch("/api/employer/profile/profile", {
+		method: "PUT",
+		body: JSON.stringify(event),
+	});
+
+	if (updated.data) {
+		refresh();
+	}
+};
+</script>
+
+<template>
+	<pre>{{ profile }}</pre>
+	<EmployerCompanyProfileTab :profile="profile" @submit="handleProfileUpdate" />
+</template>
